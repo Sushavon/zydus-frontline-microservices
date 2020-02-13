1d3f102ffeeacce327965f38c02db8ab1e8b124f
sushavonb372 <sushavonb372@INPC197TWJ>
2020-02-13 10:19:25 +0530

    Adding Product Microservice

---
+++ Product.Api/Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Product.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+
+            return GetDummyProducts();
+        }
+
+        public IEnumerable<Product> GetDummyProducts()
+        {
+            var products = new List<Product>() {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Paracetamol",
+                    Price = 1.00
+                },
+                new Product{
+                    Id = 2,
+                    Name = "Calpol",
+                    Price = 1.00
+                }
+            };
+            return products;
+        }
+    }
+}
---
+++ Product.Api/Product.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Product.Api
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+    }
+}
