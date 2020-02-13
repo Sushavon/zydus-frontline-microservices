171cd89e45d96bed64c462bdb05d72e63243e2f6
sushavonb372 <sushavonb372@INPC197TWJ>
2020-02-13 10:19:59 +0530

    Adding User Microservice

---
+++ User.Api/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace User.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(GetDummyUsers());
+        }
+
+        public IEnumerable<User> GetDummyUsers()
+        {
+            var users = new List<User>()
+            {
+                new User
+                {
+                    Id = 1,
+                    Name = "Rakesh",
+                    Email = "rakesh.jha@zydus.in"
+                },
+                new User
+                {
+                    Id = 2,
+                    Name = "Ramesh",
+                    Email = "ramesh.jha@zydus.in"
+                }
+            };
+
+            return users;
+        }
+    }
+}
---
+++ User.Api/User.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace User.Api
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
